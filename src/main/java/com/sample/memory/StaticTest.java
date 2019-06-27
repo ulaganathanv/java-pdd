166d3e8f7cc9491a2f51622b0df8bc09dacdfaad
Ulaganathan Vallinayagam <ulag.india@gmail.com>
2019-06-27 16:13:57 +0530

    Update code to change the value

--- src/main/java/com/sample/memory/StaticTest.java
+++ src/main/java/com/sample/memory/StaticTest.java
@@ -11,30 +11,28 @@ public class StaticTest {
 
     public static LinkedHashSet<Double> list = new LinkedHashSet();
 
-    public void populateList() {
-
-        for (int i = 0; i < 10000000; i++) {
-
-            if(i%1000 == 0) {
-                System.out.println("Processed Count : " + i);
-            }
-
-            double randomValue  = random();
-            if(list.contains(randomValue) == false ) {
-                list.add(randomValue);
-            }
-
-
-        }
-        System.out.println("Debug Point 2");
-    }
-
     public static void main(String[] args) throws Exception {
         System.out.println("Debug Point 1");
         Thread.sleep(10000);
         StaticTest staticTest = new StaticTest();
         staticTest.populateList();
         System.out.println("Debug Point 3");
-        Thread.sleep(1000000);
+        //Thread.sleep(1000000);
+    }
+
+    public void populateList() {
+
+        for (int count = 0; count < 200000; count++) {
+
+            if(count%1000 == 0) {
+                System.out.println("Processed Count : " + count);
+            }
+
+            double randomValue  = random();
+            if(list.contains(randomValue) == false ) {
+                list.add(randomValue);
+            }
+        }
+        System.out.println("Debug Point 2");
     }
 }
